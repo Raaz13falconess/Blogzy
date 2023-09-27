63ffbbf8c4a19e59774245d3e4ad0cfb3716c963
Raaz13falconess <72591130+Raaz13falconess@users.noreply.github.com>
2023-09-27 09:31:08 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 const sessionSecret = "blogsecretsession"
 
-const emailUser = "raaz13bharadwaj@gmail.com"
-const emailPassword = "lpupnmidntzocqdm"
+
 
 module.exports = {sessionSecret, emailPassword, emailUser}
